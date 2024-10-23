c92510fcd96ee616da9568f847b4901f500df532
common-umar <135182526+common-umar@users.noreply.github.com>
2024-10-23 23:24:40 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import pandas as pd
+import textwrap
+
+# Load and Cache the data
+@st.cache_data(persist=True)
+def getdata():
+    games_df = pd.read_csv('Games_dataset.csv', index_col=0)
+    similarity_df = pd.read_csv('sim_matrix.csv', index_col=0)
+    return games_df, similarity_df
+
+games_df, similarity_df = getdata()
+
+# Sidebar
+st.sidebar.markdown('<strong><span style="color: #8B2500;font-size: 26px;"> Game recommendation</span></strong>', unsafe_allow_html=True)
+st.sidebar.markdown('An app by [Long Do](https://doophilong.github.io/Portfolio/)')
+st.sidebar.image('pexels-pixabay-275033.jpg', use_column_width=True)
+st.sidebar.markdown('<strong><span style="color: #EE4000;font-size: 26px;">:slot_machine: Choose your game !!!</span></strong>', unsafe_allow_html=True)
+
+# HTML form for JavaScript
+st.markdown("""
+    <form id="gameForm">
+        <input type="text" id="gameInput" placeholder="Enter game title..." />
+        <input type="submit" value="Submit" />
+    </form>
+    <script src="app.js"></script>
+""", unsafe_allow_html=True)
+
+# Get query parameters
+query_params = st.experimental_get_query_params()
+default_game = query_params.get('game', [''])[0]  # Get the game title from the URL, default to empty string
+
+# Generate game list
+games_list = [''] + games_df['Title'].to_list()  # Include empty string for "Select a game" option
+
+# Safely find the index of the default game in the dataset, if provided
+if default_game in games_df['Title'].to_list():
+    default_index = games_df['Title'].to_list().index(default_game) + 1  # +1 for the empty string
+else:
+    default_index = 0  # Default to the first option if not found
+
+selected_game = st.selectbox(
+    'Select one among the 787 games from the menu: (you can type it as well)',
+    games_list, 
+    index=default_index,  # Use the calculated index with the empty string considered
+    key='default',
+    format_func=lambda x: 'Select a game' if x == '' else x
+)
+
+# Recommendations
+if selected_game:
+    link = 'https://en.wikipedia.org' + games_df[games_df.Title == selected_game].Link.values[0]
+
+    # DF query
+    matches = similarity_df[selected_game].sort_values()[1:6]
+    matches = matches.index.tolist()
+    matches = games_df.set_index('Title').loc[matches]
+    matches.reset_index(inplace=True)
+    
+    # Prepare response data
+    response_data = matches[['Title', 'Genre', 'Developer', 'Publisher', 'Plots', 'Link']].to_dict(orient='records')
+    
+    # Return JSON response
+    st.json(response_data)  # This will display JSON in the Streamlit app
+
+    # Results
+    cols = ['Genre', 'Developer', 'Publisher', 'Released in: Japan', 'North America', 'Rest of countries']
+    
+    st.markdown("# The recommended games for [{}]({}) are:".format(selected_game, link))
+    for idx, row in matches.iterrows():
+        st.markdown('### {} - {}'.format(str(idx + 1), row['Title']))
+        st.markdown(
+            '{} [[...]](https://en.wikipedia.org{})'.format(textwrap.wrap(row['Plots'][0:], 600)[0], row['Link']))
+        st.table(pd.DataFrame(row[cols]).T.set_index('Genre'))
+        st.markdown('Link to wiki page: [{}](https://en.wikipedia.org{})'.format(row['Title'], row['Link']))
+
+else:
+    st.markdown('# Game recommendation :video_game:')
+    st.text('')
+    st.markdown('> _So you have a Nintendo Switch, just finished an amazing game, and would like to get recommendations for similar games?_')
+    st.text('')
+    st.markdown("This app lets you select a game from the dropdown menu and you'll get five recommendations that are the closest to your game according to the gameplay and/or plot.")
+    st.text('')
+    st.warning(':point_left: Select a game from the dropdown menu!')
