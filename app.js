0598303b7f65055ca9fea3dae673f7fd6997b068
common-umar <135182526+common-umar@users.noreply.github.com>
2024-10-24 00:04:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-document.getElementById('gameForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission which reloads the page
-
-    // Get the value from the input
-    const gameTitle = document.getElementById('gameInput').value;
-
-    // Redirect to the Streamlit app with the game title as a query parameter
-    window.location.href = `/?game=${encodeURIComponent(gameTitle)}`; // Opens in the current tab
-});
+// app.js
+function submitGameTitle() {
+    const gameTitle = document.getElementById("gameTitle").value;
+    // Redirect to the Streamlit app with the new game title as a query parameter
+    window.location.href = `/?game=${encodeURIComponent(gameTitle)}`;
+}
