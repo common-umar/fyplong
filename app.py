fc826afb5ac3bd1ea7ecda48ef02b1dda8d512e3
common-umar <135182526+common-umar@users.noreply.github.com>
2024-10-24 19:16:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,55 +23,19 @@ ph = st.sidebar.empty()
 query_params = st.experimental_get_query_params()
 default_game = unquote(query_params.get('game', [''])[0])  # Decode URL-encoded game name
 
-games_list = [''] + games_df['Title'].to_list()  # Include empty string for "Select a game" option
+# Games list from the dataset
+games_list = games_df['Title'].to_list()  # No need for the empty string here
 
-# Safely find the index of the default game in the dataset
-if default_game in games_list:
-    default_index = games_list.index(default_game)
-else:
-    default_index = 0  # Default to the first option ("Select a game")
+# Safely find the index of the default game in the dataset, using a case-insensitive comparison
+default_game_lower = default_game.lower().strip()
+default_index = 0  # Default to "Select a game" option
+for i, game in enumerate(games_list):
+    if game.lower().strip() == default_game_lower:
+        default_index = i
+        break
 
 # Selectbox to choose game
 selected_game = ph.selectbox(
     'Select one among the 787 games from the menu: (you can type it as well)',
-    games_list,
-    index=default_index,
-    key='default',
-    format_func=lambda x: 'Select a game' if x == '' else x
-)
-
-# Recommendations
-if selected_game:
-    link = 'https://en.wikipedia.org' + games_df[games_df.Title == selected_game].Link.values[0]
-
-    # DF query
-    matches = similarity_df[selected_game].sort_values()[1:6]
-    matches = matches.index.tolist()
-    matches = games_df.set_index('Title').loc[matches]
-    matches.reset_index(inplace=True)
-    
-    # Prepare response data
-    response_data = matches[['Title', 'Genre', 'Developer', 'Publisher', 'Plots', 'Link']].to_dict(orient='records')
-    
-    # Return JSON response
-    st.json(response_data)  # This will display JSON in the Streamlit app
-
-    # Results
-    cols = ['Genre', 'Developer', 'Publisher', 'Released in: Japan', 'North America', 'Rest of countries']
-    
-    st.markdown("# The recommended games for [{}]({}) are:".format(selected_game, link))
-    for idx, row in matches.iterrows():
-        st.markdown('### {} - {}'.format(str(idx + 1), row['Title']))
-        st.markdown(
-            '{} [[...]](https://en.wikipedia.org{})'.format(textwrap.wrap(row['Plots'][0:], 600)[0], row['Link']))
-        st.table(pd.DataFrame(row[cols]).T.set_index('Genre'))
-        st.markdown('Link to wiki page: [{}](https://en.wikipedia.org{})'.format(row['Title'], row['Link']))
-
-else:
-    st.markdown('# Game recommendation :video_game:')
-    st.text('')
-    st.markdown('> _So you have a Nintendo Switch, just finished an amazing game, and would like to get recommendations for similar games?_')
-    st.text('')
-    st.markdown("This app lets you select a game from the dropdown menu and you'll get five recommendations that are the closest to your game according to the gameplay and/or plot.")
-    st.text('')
-    st.warning(':point_left: Select a game from the dropdown menu!')
+    ['Select a game'] + games_list,  # Add "Select a game" as the first option
+    index=default_index + 1 if default_in
