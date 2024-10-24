53fa77a9cfaa07568f630571da399da4aa3b5033
common-umar <135182526+common-umar@users.noreply.github.com>
2024-10-24 21:21:59 +0500

    Update apptemp.py

--- apptemp.py
+++ apptemp.py
@@ -2,8 +2,15 @@ import streamlit as st
 import pandas as pd
 import textwrap
 
-st.header("Ahoy!") hide_st_style = """ <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} header {visibility: hidden;} </style> """
-st.markdown(hide_st_style, unsafe_allow_htm1=True)
+st.header(
+    """ 
+    <style> 
+    #MainMenu {visibility: hidden;} 
+    footer {visibility: hidden;} 
+    header {visibility: hidden;} 
+    </style> 
+    """
+)
 
 # Set background color to white (RGB)
 st.markdown(
