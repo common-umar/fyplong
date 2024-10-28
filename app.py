c56bfc2a6e93947eb4cd7c6e94497357e5ef9c55
common-umar <135182526+common-umar@users.noreply.github.com>
2024-10-28 20:42:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ st.markdown(
         background-color: rgba(255, 255, 255, 0) !important;
     }
 
+    /* Make text transparent */
+    .stApp, .stMarkdown, .stText, .stTitle, .stHeader, .stCaption, .stWidget, .stButton {
+        color: rgba(0, 0, 0, 0) !important;
+    }
+
   
     .stAppHeader,
     .stToolbarActionButton,
