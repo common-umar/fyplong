b5155eb410a21585a671ba8ce07a1b4ce87888cc
common-umar <135182526+common-umar@users.noreply.github.com>
2024-11-03 20:28:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -52,66 +52,66 @@ def getdata():
 
 games_df, similarity_df = getdata()
 
-# Standardize game titles to lowercase for comparison
+# Standardize game titles and genres to lowercase for comparison
 games_df['lower_title'] = games_df['Title'].str.lower()
+games_df['lower_genre'] = games_df['Genre'].str.lower()
 
-# Fetch game from URL query parameters
+# Fetch game or genre from URL query parameters
 query_params = st.experimental_get_query_params()
-default_game = query_params.get('game', [''])[0]  # Default to an empty string if 'game' is not in query
-
-# Lowercase the default game for comparison
-default_game_lower = default_game.lower()
+default_game_or_genre = query_params.get('game', [''])[0].lower()  # Default to empty string if 'game' not in query
 
 games_list = [''] + games_df['Title'].to_list()  # Include empty string for "Select a game" option
+genres_list = games_df['Genre'].unique().tolist()
 
-# Find the index of the default game in the dataset
-if default_game_lower in games_df['lower_title'].values:
-    default_index = games_df['lower_title'].tolist().index(default_game_lower) + 1  # +1 for the empty string
+# Check if input matches a game title or genre
+if default_game_or_genre in games_df['lower_title'].values:
+    selected_type = 'game'
+    default_index = games_df['lower_title'].tolist().index(default_game_or_genre) + 1  # +1 for empty string
+elif default_game_or_genre in games_df['lower_genre'].values:
+    selected_type = 'genre'
 else:
-    default_index = 0  # Default to the first option ("Select a game")
+    selected_type = None
+    default_index = 0  # Default to first option ("Select a game")
 
-# Selectbox to choose game
-selected_game = st.selectbox(
-    'Select one among the 787 games from the menu: (you can type it as well)',
+# Selectbox for game selection
+selected_game_or_genre = st.selectbox(
+    'Select a game or enter a genre for recommendations:',
     games_list,
     index=default_index,
     key='default',
-    format_func=lambda x: 'Select a game' if x == '' else x
+    format_func=lambda x: 'Select a game or genre' if x == '' else x
 )
 
-# Recommendations
-if selected_game:
-    link = 'https://en.wikipedia.org' + games_df[games_df.Title == selected_game].Link.values[0]
-
-    # DF query
-    matches = similarity_df[selected_game].sort_values()[1:6]
-    matches = matches.index.tolist()
-    matches = games_df.set_index('Title').loc[matches]
-    matches.reset_index(inplace=True)
+# Show recommendations based on game or genre
+if selected_game_or_genre:
+    if selected_type == 'game':
+        # Get recommendations based on selected game
+        link = 'https://en.wikipedia.org' + games_df[games_df.Title == selected_game_or_genre].Link.values[0]
+        matches = similarity_df[selected_game_or_genre].sort_values()[1:6].index.tolist()
+        matches = games_df.set_index('Title').loc[matches]
+        matches.reset_index(inplace=True)
+        recommendations = matches[['Title', 'Genre', 'Developer', 'Publisher', 'Plots', 'Link']].to_dict(orient='records')
+        
+        st.markdown("# Recommended games for [{}]({}) are:".format(selected_game_or_genre, link))
+        
+    elif selected_type == 'genre':
+        # Get recommendations based on selected genre
+        genre_matches = games_df[games_df['lower_genre'] == default_game_or_genre].sample(5)  # Random 5 games in genre
+        recommendations = genre_matches[['Title', 'Genre', 'Developer', 'Publisher', 'Plots', 'Link']].to_dict(orient='records')
+        
+        st.markdown(f"# Recommended games in genre '{selected_game_or_genre.capitalize()}':")
     
-    # Prepare response data
-    response_data = matches[['Title', 'Genre', 'Developer', 'Publisher', 'Plots', 'Link']].to_dict(orient='records')
-    
-    # Return JSON response
-  #  st.json(response_data)  # This will display JSON in the Streamlit app
-
-    # Results
-    cols = ['Genre', 'Developer', 'Publisher', 'Released in: Japan', 'North America', 'Rest of countries']
-    
-    st.markdown("# The recommended games for [{}]({}) are:".format(selected_game, link))
-    for idx, row in matches.iterrows():
-        st.markdown('### {} - {}'.format(str(idx + 1), row['Title']))
-        st.markdown(
-            '{} [[...]](https://en.wikipedia.org{})'.format(textwrap.wrap(row['Plots'][0:], 600)[0], row['Link']))
-        st.table(pd.DataFrame(row[cols]).T.set_index('Genre'))
-        st.markdown('Link to wiki page: [{}](https://en.wikipedia.org{})'.format(row['Title'], row['Link']))
+    # Display recommendations
+    for idx, game in enumerate(recommendations):
+        st.markdown(f"### {idx + 1}. {game['Title']} - {game['Genre']}")
+        st.markdown('{} [[...]](https://en.wikipedia.org{})'.format(textwrap.wrap(game['Plots'], 600)[0], game['Link']))
+        cols = ['Genre', 'Developer', 'Publisher']
+        st.table(pd.DataFrame([game]).T.loc[cols])
+        st.markdown(f"Link to wiki page: [{game['Title']}]({game['Link']})")
 
 else:
-    st.markdown('# Game recommendation :video_game:')
+    # Default instructions if no game or genre is selected
+    st.markdown('# Game Recommendation :video_game:')
     st.text('')
-    st.markdown('> _So you have a Nintendo Switch, just finished an amazing game, and would like to get recommendations for similar games?_')
-    st.text('')
-    st.markdown("This app lets you select a game from the dropdown menu and you'll get five recommendations that are the closest to your game according to the gameplay and/or plot.")
-    st.text('')
-    st.warning(':point_left: Select a game from the dropdown menu!')
-
+    st.markdown('> _Select a game or genre from the dropdown menu for personalized recommendations._')
+    st.warning(':point_left: Select a game or enter a genre in the input box!')
